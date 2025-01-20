62b94a09a5d938de724fd73516443cd52c43ebf6
DenisShalov <d24783569@gmail.com>
2025-01-20 18:15:35 +0300

    Addition1

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-let numberNotes = 5;
-
-
 
 const MOCK_NOTES = [
     {
@@ -8,6 +5,20 @@ const MOCK_NOTES = [
         title: 'Работа с формами',
         content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
         color: 'green',
+        isFavorite: false,
+    },
+    {
+        id: 2,
+        title: '1',
+        content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
+        color: 'blue',
+        isFavorite: false,
+    },
+    {
+        id: 3,
+        title: '2',
+        content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
+        color: 'red',
         isFavorite: false,
     },
     // ...
@@ -26,7 +37,7 @@ const model = {
     notes: MOCK_NOTES,
     addNote(title, content, color, isFavorite) {
         const id = Math.random()
-        const newNote = { id, title, content, color, isFavorite }
+        const newNote = { id, title, content, color, isFavorite}
         this.notes.unshift(newNote)
         view.renderNotes(this.notes);
         this.coutnNotes();
@@ -38,6 +49,24 @@ const model = {
         this.coutnNotes();
     },
 
+    // ..._____________________________________________________________________________________________
+    isShowOnlyFavorite: false,
+    toggleShowOnlyFavorite(isShowOnlyFavorite) {
+        this.isShowOnlyFavorite = isShowOnlyFavorite;
+    },
+    updateNotesView() {
+        if (this.isShowOnlyFavorite) {
+            const notesToRender = this.notes.filter((note) => note.isFavorite === this.isShowOnlyFavorite)
+            view.renderNotes(notesToRender);
+            this.coutnNotes();
+        } else {
+            view.renderNotes(this.notes);
+            this.coutnNotes();
+        }
+    },
+    //___________________________________________________________________________________________________
+
+
     coutnNotes() {
         const numberNotesElement = document.getElementById('number-of-notes');
         numberNotesElement.textContent = ` Всего заметок: ${model.notes.length} `;
@@ -47,33 +76,41 @@ const model = {
 const view = {
     init() {
         this.renderNotes(model.notes);
-
+        model.coutnNotes()
         const form = document.querySelector('.form');
         const inputTitle = document.querySelector('.input-title');
         const inputDescription = document.getElementById('note-description');
         const stack = document.querySelectorAll('.radio-button input[type = "radio"]');
+        const inputFilter = document.querySelector('.filter-box input[type = "checkbox"]');
 
+        //фильтр по избранныи элементам
+        inputFilter.addEventListener('change', function (event) {
+            event.preventDefault()
+            model.toggleShowOnlyFavorite(this.checked);
+            model.updateNotesView();
+        });
+
+        //Работа с формой, добавление новой заметки
         form.addEventListener('submit', function (event) {
             event.preventDefault();
             const title = inputTitle.value;
             const description = inputDescription.value;
-            controller.addNote(title, description);
+            let color = "";
+            const stackArr = new Array(...stack);
+            for (const element of stackArr) {
+                if (element.checked === true) {
+                    color = colors[element.value];
+                }
+            }
+            controller.addNote(title, description, color);
 
             inputTitle.value = ''
             inputDescription.value = ''
-
-            const stackArr = new Array(...stack);
-            for (const element of stackArr) {
-                if(element.checked === true)
-                    {
-                        console.log(element.value);
-                    }
-            }
         });
 
+        //Удаление заметки
         const list = document.querySelector('.notes-list');
         list.addEventListener('click', function (event) {
-
             if (event.target.classList.contains("delete-button")) {
                 const noteId = +event.target.closest('li').id;
                 controller.deleteNote(noteId);
@@ -82,6 +119,7 @@ const view = {
     },
 
 
+//Отрисовка заметки в браузере 
 
     renderNotes(notes) {
         const list = document.querySelector('.notes-list');
@@ -89,10 +127,10 @@ const view = {
 
         for (const note of notes) {
             noteHTML += `<li id="${note.id}" class="note">
-                    <b class="note-title">${note.title}
+                    <b class="note-title" style="background-color : ${note.color}">${note.title}
                         <b>
                             <label class="FavoriteNoteLabel">
-                                <input class="isFavoriteNote" type="checkbox"></input>
+                                <input class="isFavoriteNote" type="checkbox" ${note.isFavorite ? 'checked' : ''}></input>
                                 <span class="imageNote"></span>
                             </label>
                             <button class="delete-button" type="button"><img 
@@ -100,12 +138,29 @@ const view = {
                             src="images/icons/trash.svg" alt=""></button>
                         </b>
                     </b>
-                    <p class="note-description">${note.content}</li>`
+                    <p class="note-description">${note.content}</p>
+                    
+                    </li>`
+
         }
         list.innerHTML = noteHTML;
-
+        const checkboxes = document.querySelectorAll('.isFavoriteNote');
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', function(event) {
+                event.preventDefault()
+                const noteId = +this.closest('li').id; // Получаем ID заметки
+                const note = model.notes.find(n => n.id === noteId); // Находим заметку по ID
+                if (note) {
+                    note.isFavorite = this.checked; // Обновляем состояние isFavorite
+                    view.displayMessage(`Заметка "${note.title}" была ${this.checked ? 'добавлена в избранное!' : 'удалена из избранного!'} `);
+                }
+                model.updateNotesView();
+            })})
     },
+
+    //Отрисовка сообщений
     displayMessage(message, isError = false) {
+        
         const messageBox = document.querySelector('.message-box')
         messageBox.textContent = message
         if (isError) {
@@ -119,26 +174,29 @@ const view = {
 }
 
 const controller = {
-    addNote(title, content) {
-        if(title.length<=50){
-        if (title.trim() !== '' && content.trim() !== '') {
-            model.addNote(title, content);
-            view.displayMessage('Заметка добавлена успешно!');
+    addNote(title, content, color) {
+        if (title.length <= 50) {
+            if (title.trim() !== '' && content.trim() !== '') {
+                model.addNote(title, content, color);
+                view.displayMessage('Заметка добавлена успешно!');
+            } else {
+                view.displayMessage('Заполните все поля!', true)
+            }
         } else {
-            view.displayMessage('Заполните все поля!', true)
+            view.displayMessage('Длина заголовка не должна превышать 50 символов!', true)
         }
-    }else{
-        view.displayMessage('Длина заголовка не должна превышать 50 символов!', true)}
     },
     deleteNote(noteId) {
         model.deleteNote(noteId);
         view.displayMessage("Заметка успешно удалена!");
     }
+
+
 }
 
 function init() {
     view.init()
-    model.coutnNotes()
+   
 
 }
 
